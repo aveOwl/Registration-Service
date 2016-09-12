53356169f73311e8fc9ed59dd9965a9ddb92ad22
Ave Owl <redowlave@gmail.com>
2016-09-12 07:46:03 +0300

    templates update

--- web/src/main/java/com/registration/Application.java
+++ web/src/main/java/com/registration/Application.java
@@ -2,13 +2,35 @@ package com.registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.AppCacheManifestTransformer;
+import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
 
 @SpringBootApplication
 @EnableAsync
-public class Application {
+public class Application extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public ResourceUrlEncodingFilter filter() {
+        return new ResourceUrlEncodingFilter();
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**") // handle all paths
+                .addResourceLocations("classpath:/static/", "/webjars/*")
+                .resourceChain(true)
+                .addResolver(
+                        new VersionResourceResolver()
+                                .addFixedVersionStrategy("1.0.SNAPSHOT", "/**/*.js") // for .js files prepend with version
+                                .addContentVersionStrategy("/**")) // for css files - hash strategy
+                .addTransformer(new AppCacheManifestTransformer()); // code can be used offline
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
